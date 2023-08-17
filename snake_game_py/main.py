3dbd047d715a9121a455add8ebef2acae2e74505
femto <femtowin@gmail.com>
2023-08-17 18:57:23 +0800

    food

--- snake_game_py/main.py
+++ snake_game_py/main.py
@@ -1,6 +1,15 @@
 """main.py: Main entry point for the Snake game using pygame."""
 import sys
-sys.path.append("..")
+import os
+
+# Get the current file's directory
+current_dir = os.path.dirname(os.path.abspath(__file__))
+
+# Calculate the parent directory
+parent_dir = os.path.dirname(current_dir)
+
+# Append the parent directory to sys.path
+sys.path.append(parent_dir)
 import pygame
 from snake_game_py.game import Game
 from snake_game_py.snake import Snake
